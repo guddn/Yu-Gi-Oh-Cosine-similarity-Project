783186c1bd89cb293944a8190102a6f3ab8dc179
helloguddn (PC helloguddn) <helloguddn@ajou.ac.kr>
2025-12-13 01:32:30 +0900

    modify card_names and readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Yu-Gi-Oh-Cosine-similarity-Project
+
+## 유희왕에서 사용되는 카드들을 임베딩 후 Cosine similarity를 적용하는 프로젝트
+- 모든 카드를 임베딩하는 것은 불가능 -> 기준으로 선정 필요
+- 시각화 필요
--- cosine_sim.py
+++ cosine_sim.py
@@ -3,7 +3,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 def compute_cosine_similarity():
     # Load card data
-    card_names = ["섬도희-카가리", "섬도희-시즈쿠", "섬도희-하야테"]
+    card_names = ["섬도희-카가리", "섬도희-시즈쿠", "섬도희-하야테", "섬도희-카이나"]
 
     for card_name in card_names:
         card_data_object = load_card_data(card_name)
